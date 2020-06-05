cef495f642dfe01f57745be2b85dd588de10aa3c
hyy <472118643@qq.com>
2020-06-05 15:21:49 +0800

    reigster + forget+ login  注册

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 // import * as ElementUI from 'element-ui'
-import Vue from 'vue'
-import InputDialog from './tsy/Base/InputDialog.vue';
-import UserGroupTree from './tsy/User/UserGroupTree.vue';
+import Vue from "vue";
+import InputDialog from "./tsy/Base/InputDialog.vue";
+import UserGroupTree from "./tsy/User/UserGroupTree.vue";
+
+import Register from "./tsy/User/Regist.vue";
+import ForGet from "./tsy/User/Forget.vue";
+import Login from "./tsy/User/Login.vue";
 
 // Vue.use(ElementUI)
 
 const components: any = {
-    InputDialog,
-    UserGroupTree
-}
+  InputDialog,
+  UserGroupTree,
+  Register,
+  ForGet,
+  Login,
+};
 
 export default {
-    install: (vue: any, opt: any) => {
-        for (let x in components) {
-            vue.component('Ctsy' + x, components[x])
-        }
-        // vue.component('InputDialog', InputDialog);
+  install: (vue: any, opt: any) => {
+    for (let x in components) {
+      vue.component("Ctsy" + x, components[x]);
     }
-}
+    // vue.component('InputDialog', InputDialog);
+  },
+};
