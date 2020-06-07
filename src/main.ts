43afa38565c4764d4e319d68dd5828337394d360
hyy <472118643@qq.com>
2020-06-07 18:36:44 +0800

    关闭appid 注释

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import * as ElementUI from 'element-ui'
-import Ctsy from './index'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import * as ElementUI from "element-ui";
+import Ctsy from "./index";
 
-import { ApiConfig } from '@ctsy/api-sdk'
+import { ApiConfig } from "@ctsy/api-sdk";
 
-// ApiConfig.AppID = 'dev';
-// ApiConfig.Secret = 'dev2930sf9fwopfwe9';
-// ApiConfig.Key = 'dev';
+ApiConfig.AppID = "dev";
+ApiConfig.Secret = "dev2930sf9fwopfwe9";
+ApiConfig.Key = "dev";
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
-Vue.use(Ctsy)
+Vue.use(Ctsy);
 
 Vue.config.productionTip = false;
 
@@ -21,4 +21,4 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
