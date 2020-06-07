a1822896e9ef28fc49981ab13da1b911ba7c1fdf
hyy <472118643@qq.com>
2020-06-07 15:00:51 +0800

    新增 emailcode reg

--- src/lib/Reg/index.ts
+++ src/lib/Reg/index.ts
@@ -9,7 +9,7 @@ const Reg: { [index: string]: any } = {
   phone: /^[+86]{0,}1\d{10}$/, //电话号码
   messageCode: /^\d{4}$/, //短信验证码
   email: /^\w+@[a-z0-9]+(\.[a-z]+){1,3}/, //邮箱
-  emailCode: "", //邮箱验证码 无
+  emailCode: /^\d{4}$/, //邮箱验证码 无
   pwd: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/, //密码必须包含数字字母
 };
 
