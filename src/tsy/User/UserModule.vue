5c26c35efdc3d683f947aca14daf6077a1d3517d
hyy <472118643@qq.com>
2020-06-08 20:36:43 +0800

    新增一个 包含Login\register 的组件

---
+++ src/tsy/User/UserModule.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-dialog
+      title=""
+      :visible.sync="visible"
+      width="30%"
+      :show-close="false"
+    >
+
+      <span slot="footer">
+        <el-button @click="visible = false">返回</el-button>
+      </span>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class Name extends Vue {
+  @Prop({ default: true })
+  visible?: boolean;
+}
+</script>
+
+<style lang="scss" scoped >
+</style>
