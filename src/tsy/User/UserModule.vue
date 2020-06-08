9204034f1a69668b8af27c887b9b271bdf83b12f
hyy <472118643@qq.com>
2020-06-08 21:10:24 +0800

    将 login ，register,forget,组装

--- src/tsy/User/UserModule.vue
+++ src/tsy/User/UserModule.vue
@@ -3,13 +3,42 @@
     <el-dialog
       title=""
       :visible.sync="visible"
-      width="30%"
+      width="60%"
       :show-close="false"
+      :close-on-click-modal="false"
     >
 
-      <span slot="footer">
-        <el-button @click="visible = false">返回</el-button>
-      </span>
+      <el-tabs
+        v-model="activeName"
+        @tab-click="handleClick"
+      >
+        <el-tab-pane
+          label="登录"
+          name="login"
+        >
+          <CtsyLogin key="login"></CtsyLogin>
+
+        </el-tab-pane>
+        <el-tab-pane
+          label="注册"
+          name="register"
+        >
+          <CtsyRegister
+            :registerProps="show"
+            key="forget"
+          ></CtsyRegister>
+
+        </el-tab-pane>
+        <el-tab-pane
+          label="找回密码"
+          name="forget"
+        >
+          <CtsyForGet key="register"> </CtsyForGet>
+
+        </el-tab-pane>
+
+      </el-tabs>
+
     </el-dialog>
 
   </div>
@@ -21,6 +50,16 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 export default class Name extends Vue {
   @Prop({ default: true })
   visible?: boolean;
+
+  activeName: string = "login";
+
+  show: any = {
+    Avatar: false,
+    IsPhone: true,
+    IsEmail: true
+  };
+
+  handleClick() {}
 }
 </script>
 
