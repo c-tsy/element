b3d19cb9b0899d8541c6816c041abf7716008f6a
hyy <472118643@qq.com>
2020-06-08 18:46:03 +0800

    demo 文件 用于测试组件封装效果

--- src/tsy/demo.vue
+++ src/tsy/demo.vue
@@ -1,7 +1,26 @@
 <template>
   <el-row>
-    <el-col :span="24">
-      <CtsyRegister></CtsyRegister>
+    <el-col :span="12">
+      <!-- <CtsyRegister
+        :registerProps="show"
+        :visible='true'
+      ></CtsyRegister> -->
+
+      <CtsyLogin :visible='true'></CtsyLogin>
+
+      <!-- <CtsyForGet :visible="true">      </CtsyForGet> -->
+
+    </el-col>
+    <el-col :span="12">
+      {{info}}
+      <CtsyPersonInfo
+        :lable=test
+        :data="info"
+        align="center"
+        :avatar_url='info.Avatar'
+      >
+
+      </CtsyPersonInfo>
     </el-col>
   </el-row>
 </template>
@@ -9,8 +28,49 @@
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
 
-@Component({})
-export default class UserList extends Vue {}
+@Component({
+  components: {}
+})
+export default class UserList extends Vue {
+  test: any = [
+    {
+      prop: "Name",
+      lable: "姓名:"
+    },
+    {
+      prop: "Nick",
+      lable: "昵称:"
+    },
+    {
+      prop: "Sex",
+      lable: "性别:"
+    },
+    {
+      prop: "Status",
+      lable: "状态:"
+    }
+  ];
+
+  show: any = {
+    Avatar: false,
+    IsPhone: false,
+    IsEmail: true
+  };
+
+  m: any = "";
+  arr: any = [
+    {
+      name: "张三"
+    },
+    {
+      name: "李四"
+    }
+  ];
+
+  get info() {
+    return this.$store.getters.info;
+  }
+}
 </script>
 
 <style lang="less" scoped>
