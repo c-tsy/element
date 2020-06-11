0dc910ef786e6fb5178d67f7c753b1604af16407
hyy <472118643@qq.com>
2020-06-11 11:42:24 +0800

    新增  通过循环挂载filter的方法

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import store from "./store";
 import * as ElementUI from "element-ui";
 import Ctsy from "./index";
 
+import * as filters from "@/filters";
+
 import { ApiConfig } from "@ctsy/api-sdk";
+
+import User from "@ctsy/api-sdk/dist/modules/User";
+
+// User.Sex;
 
 // ApiConfig.AppID = "dev";
 // ApiConfig.Secret = "dev2930sf9fwopfwe9";
@@ -16,6 +22,10 @@ Vue.use(ElementUI);
 Vue.use(Ctsy);
 
 Vue.config.productionTip = false;
+
+Object.keys(filters).forEach((e: any) => {
+  Vue.filter(e, (filters as { [key: string]: Function })[e]);
+});
 
 new Vue({
   router,
