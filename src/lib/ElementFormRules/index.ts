702f2dcdbe6e3bcbae0d80225c06ebb03b448fd6
hyy <472118643@qq.com>
2020-06-11 15:53:49 +0800

    class 改为 对象

--- src/lib/ElementFormRules/index.ts
+++ src/lib/ElementFormRules/index.ts
@@ -4,8 +4,8 @@ namespace ElementFromRules {
   /**
    * elementui from  表单校验rules 规则
    */
-  export class defalultRules {
-    Name: Array<any> = [
+  export const defalultRules = {
+    Name: [
       {
         required: true,
         message: "姓名不能为空",
@@ -14,8 +14,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValiteName_fun,
         trigger: "blur",
       },
-    ];
-    NickName: Array<any> = [
+    ],
+    NickName: [
       {
         required: true,
         message: "昵称不能为空",
@@ -24,8 +24,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValiteNick_fun,
         trigger: "blur",
       },
-    ];
-    Account: Array<any> = [
+    ],
+    Account: [
       {
         required: true,
         message: "账号不能为空",
@@ -34,8 +34,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValiteAccount_fun,
         trigger: "blur",
       },
-    ];
-    Phone: Array<any> = [
+    ],
+    Phone: [
       {
         required: true,
         message: "电话号码不能为空",
@@ -44,8 +44,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValitePhone_fun,
         trigger: "blur",
       },
-    ];
-    MessageCode: Array<any> = [
+    ],
+    MessageCode: [
       {
         required: true,
         message: "短信验证码不能为空",
@@ -54,8 +54,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValiteMessage_fun,
         trigger: "blur",
       },
-    ];
-    Email: Array<any> = [
+    ],
+    Email: [
       {
         required: true,
         message: "邮箱不能为空",
@@ -64,8 +64,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValiteEmail_fun,
         trigger: "blur",
       },
-    ];
-    EmailCode: Array<any> = [
+    ],
+    EmailCode: [
       {
         required: true,
         message: "邮箱验证码不能为空",
@@ -74,8 +74,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValiteEmailCode_fun,
         trigger: "blur",
       },
-    ];
-    PWD: Array<any> = [
+    ],
+    PWD: [
       {
         required: true,
         message: "密码不能为空",
@@ -84,8 +84,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValitePWD_fun,
         trigger: "blur",
       },
-    ];
-    RPWD: Array<any> = [
+    ],
+    RPWD: [
       {
         required: true,
         message: "重复密码不能为空",
@@ -94,8 +94,8 @@ namespace ElementFromRules {
         validator: CustomValidate.ValitePWD_fun,
         trigger: "blur",
       },
-    ];
-  }
+    ],
+  };
 }
 
 export default ElementFromRules;
