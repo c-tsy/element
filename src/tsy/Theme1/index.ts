1ee488219a89036fb184f991c5302277f9d9dc96
hyy <472118643@qq.com>
2020-06-29 17:47:48 +0800

    删除无用的helloworld 文件，新增主题一，统一用index.ts 引入组件

---
+++ src/tsy/Theme1/index.ts
@@ -0,0 +1,11 @@
+import Register from "./Regist.vue";
+import Forget from "./Forget.vue";
+import Login from "./Login.vue";
+import UserModule_theme1 from "./UserModule.vue";
+
+export default {
+  Register,
+  Forget,
+  Login,
+  UserModule_theme1,
+};
