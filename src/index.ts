4adfa8e6429b9e29059c986ff816852c32ff128f
hyy <472118643@qq.com>
2020-06-29 17:45:45 +0800

    修改引入文件的方式

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,22 @@
 // import * as ElementUI from 'element-ui'
-import Vue from "vue";
 import InputDialog from "./tsy/Base/InputDialog.vue";
 import UserGroupTree from "./tsy/User/UserGroupTree.vue";
 
-import Register from "./tsy/User/Regist.vue";
-import Forget from "./tsy/User/Forget.vue";
-import Login from "./tsy/User/Login.vue";
-import PersonInfo from "./tsy/User/Person.vue";
-import UserModule from "./tsy/User/UserModule.vue";
-
-// Vue.use(ElementUI)
+import theme1 from "./tsy/Theme1";
+import theme2 from "./tsy/Theme2";
 
 const components: any = {
+  ...theme1,
+  ...theme2,
   InputDialog,
   UserGroupTree,
-  Register,
-  Forget,
-  Login,
-  PersonInfo,
-  UserModule,
 };
 
 export default {
   install: (vue: any, opt: any) => {
     for (let x in components) {
+      console.log(x);
+
       vue.component("Ctsy" + x, components[x]);
     }
     // vue.component('InputDialog', InputDialog);
